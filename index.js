6588985ab72ecda40bff5fb79944f66063614fe0
shoelfikar <sulfikardi@wgroup.id>
2022-02-19 11:51:40 +0700

    init project

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log('hello')
+var name = 'sulfikardi'
+let address = "jakarta"
+const angka = 10
+
+let status = true
+
+
+
+
+name = 'hello'
+address = 'bandung'
+
+// let address = 'sulfikardi'
+
+
+console.log(address)
